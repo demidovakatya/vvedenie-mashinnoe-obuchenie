2c16412c53cdbfded1a7814e0cb33ec9bcd1b406
Katya Demidova <demidovakatya@gmail.com>
2016-10-14 21:28:27 +0300

    update

--- cleaning-up/makelinkstxt.py
+++ cleaning-up/makelinkstxt.py
@@ -1,31 +1,60 @@
 import os
 import re
 
-os.chdir('..')
-files = [file for file in os.listdir() if file.endswith('md')]
+def get_files():
+    if os.path.abspath(os.curdir).endswith('/cleaning-up'):
+        os.chdir('..')
+    files = [file for file in os.listdir('.') if file.endswith('md')]
+    return files
 
-lines = []
 
-for f in files:
-    with open(f) as file:
-        for line in file:
-            lines.append(line.strip())
+def get_lines(files):
+    lines = []
+
+    for f in files:
+        with open(f) as file:
+            for line in file:
+                lines.append(line.strip())
+    return lines
+
+def line_is_valid(line):
+    is_valid = (line != '') and not (line.startswith('#'))
+    try:
+        s, e = re.search('https?://[^)]*', line).span()
+    except AttributeError:
+        is_valid = False # has no link
+    return is_valid
+
+def get_link_from_line(line):
+    s, e = re.search('https?://[^)]*', line).span()
+    link = line[s:e]
+    return link
+
+def get_text_from_line(line):
+    text = re.sub('^\* |\[|\]|\(https?://[^\)]*\)', '', line)
+    return text
+
+files = get_files()
+lines = get_lines(files)
 
 len_lines = len (lines)
 print("%s lines in %s files were read" % (len_lines, len(files)))
 
-print("Removing empty lines...")
-lines = [line for line in lines if not line == '']
-print("%s empty lines were removed" % (len_lines - len(lines)))
+print("Removing invalid lines...")
+lines = [line for line in lines if line_is_valid(line)]
+print("%s invalid lines were removed" % (len_lines - len(lines)))
+len_lines = len (lines)
+
+with open('cleaning-up/lines.csv', 'w') as file:
+    for line in lines:
+        file.write(line + ';\n')
 
 links = []
-for line in lines:  
-    if len(re.findall(r"\(.*\)", line)) != 0:
-        text = re.sub(r"\(.*\)|\[|\]|\* |;|\.$|:.*:|\+", "", line).strip()
-        link = re.sub(r"\(|\)", "", re.findall(r"\(.*\)", line)[0])
-        links.append([text, link])
+for line in lines:
+    item = (get_link_from_line(line), get_text_from_line(line))
+    links.append(item)
+    with open('cleaning-up/links.csv', 'w') as file:
+        for item in links:
+            file.write('' + item[0] + '\t' + item[1] + '\n')
 
-print("Saving to file...")
-with open('cleaning-up/links.csv', 'w') as file:
-    for item in links:
-        file.write('"' + item[0] + '","' + item[1] + '";\n')
+
