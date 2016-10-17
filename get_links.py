6193ea7110e6d683f75133813e4c6356a090c99f
Katya Demidova <demidovakatya@gmail.com>
2016-10-18 01:01:21 +0300

    improved links import

--- get_links.py
+++ get_links.py
@@ -1,16 +1,16 @@
 import os, re
 
 def get_files():
-    files = [file for file in os.listdir('./src') if file.endswith('md')]
+    files = [file for file in os.listdir('./md') if file.endswith('md')]
     return files
 
 def get_lines(file):
-    with open(os.path.join('src', file)) as f:
+    with open(os.path.join('md', file)) as f:
         lines = [line.strip() for line in f.readlines()]
     return lines
 
 def line_is_valid(line):
-    is_valid = (line != '') and not (line.startswith('#'))
+    is_valid = (line != '') and not (line.startswith(('#', '**', '[!', '<')))
     try:
         s, e = re.search('https?://[^)]*', line).span()
     except AttributeError:
@@ -18,20 +18,34 @@ def line_is_valid(line):
     return is_valid
 
 def get_link_from_line(line):
-    s, e = re.search('https?://[^)]*', line).span()
-    link = line[s:e]
-    return link
+    try:
+        s, e = re.search('\(https?://[^)]*', line).span()
+        link = line[s + 1:e]
+        return link
+    except AttributeError:
+        s, e = re.search('https?://[^)]*', line).span()
+        link = line[s:e]
+        return link
 
 def get_text_from_line(line):
     # remove urls
     text = re.sub('\(https?://[^\)]*\)', '', line)
+    # remove urls in link texts
+    text = re.sub('\[https?://|/?\]', '', text)
+    # remove http/https
+    text = re.sub('https?://', '', text)
+    # remove brackets
+    text = re.sub('\[|\]', '', text)
     # remove punctuation
-    text = re.sub('^[\*\+] |\[|\]|[\.;,]$', '', text)
+    text = re.sub('^[\*\+] |[\.;,]$', '', text)
     text = re.sub(':[\w_]*:', '', text)
+    # remove extra spaces
     text = re.sub('\s+', ' ', text)
+    # make quotes safe
     text = re.sub('"', '\'', text)
     text = text.strip()
     return text
+
 
 files = get_files()
 
@@ -44,10 +58,6 @@ with open(os.path.join(data, 'links.csv'), 'w') as f:
     f.flush()
 for file in files:
     tags = re.sub('\.md', '', file)
-    # if tags == 'README':
-    #     tags = 'machinelearning'
-    # if tags == 'spec-recommendations':
-    #     tags = 'specialization'
 
     lines = [line for line in get_lines(file) if line_is_valid(line)]
     all_lines += lines
