d330f86092b204f9802d6e6b9c8c197d262d0619
Katya Demidova <demidovakatya@gmail.com>
2016-10-17 23:00:01 +0300

    added tags manipulations

--- process_links.py
+++ process_links.py
@@ -24,13 +24,17 @@ array(['algorithms', 'code-editors', 'datasets', 'extra-course-materials',
 """
 
 def save_updated_links(df):
-    files = os.listdir(data)
+    upd = 'updlinks'
+    if not os.path.exists(os.path.join(data, upd)):
+        os.mkdir(os.path.join(data, upd))
+    files = os.listdir(os.path.join(data, upd))
     n = sum([f.startswith('updlinks') for f in files])
-    updlinkscsv = os.path.join(data, 'updlinks_{:0>3}.csv'.format(n))
+    updlinkscsv = os.path.join(data, upd, 'updlinks_{:0>3}.csv'.format(n))
     df.to_csv(updlinkscsv, index = False)
     print('Saved to: %s' % updlinkscsv)
 
 def replace_tags(df):
+    save_updated_links(df)                
     replacements = {
                     'code-editors' : 'codeeditors',
                     'extra-course-materials' : 'extramaterials',
@@ -38,12 +42,46 @@ def replace_tags(df):
                     'linalg' : 'linearalgebra',
                     'neural-nets' : 'neuralnets',
                     'README' : 'machinelearning',
+                    'reddit' : 'social',
                     'spec-recommendations' : 'extramaterials',
                     }
     df = df.replace({'tags' : replacements})    
+    return df
+
+
+def add_url_base(df):
     save_updated_links(df)                
+    df['base'] = [(lambda u: re.sub('https?://|/.*$|www\.', '', u))(u) for u in df.url]
+    return df
+
+def add_tag(df, tag, tag_sites):
+    save_updated_links(df) 
+    for i in df.index:
+        try:
+            df.base[i]
+        except AttributeError:
+            df = add_url_base(df)
+    if df.base[i] in tag_sites and not tag in df.tags[i].split():
+        df.tags[i] += ' {}'.format(tag)
+    return df
+
+tags_to_be_added = {
+    'mooc' :  [
+                'coursera.org', 'datacamp.com', 
+                'edx.org', 'ocw.mit.edu', 'stepic.org', 'udacity.com'
+              ],
+    'social' :  [
+                'reddit.com', 'telegram.me', 'vk.com'
+                ]               
+    }
+
+def add_tags(df, tags_to_be_added):
+    tags = list
+    for tag, sites in tags_to_be_added.items():
+        df = add_tag(df, tag, sites)
     return df
 
 df_ = df.copy()
 df = replace_tags(df)
-save_updated_links(df)
+df = add_url_base(df)
+df = add_tags(df, tags_to_be_added)
